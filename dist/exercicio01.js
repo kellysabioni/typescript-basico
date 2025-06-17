75ce8da36b2cc4042793b06f8921566b5addccad
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-17 11:38:27 -0300

    Atualiza

--- dist/exercicio01.js
+++ dist/exercicio01.js
@@ -1,1 +1,12 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const modulos_1 = require("./modulos");
+function exibirCor(nome, cor) {
+    if (cor) {
+        return ` ${nome} escolheu ${cor}.`;
+    }
+    return ` ${nome} não escolheu nada.`;
+}
+console.log(exibirCor("Kelly", "Preto"));
+console.log(exibirCor("Essa pessoa"));
+(0, modulos_1.separador)();
