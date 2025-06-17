fd1d30593f7e5ffe66e06c440671eae0089ac361
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-17 09:17:44 -0300

    Atualiza diretorio para salvar arquivos compilados

--- 01-hello-world.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var nome = "Deskelly Sabioni";
-console.log("Ol\u00E1, ".concat(nome, "! Bem vindo ao TS!"));
---
+++ dist/01-hello-world.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const nome = "Kelly";
+console.log(`Olá, ${nome}! Bem vindo ao TS!`);
