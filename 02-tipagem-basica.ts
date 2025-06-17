625422e9b64bb436acc36597eedd6defa6521486
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-17 09:58:00 -0300

    Atualiza com definição de tipo

--- 02-tipagem-basica.ts
+++ 02-tipagem-basica.ts
@@ -1,5 +1,4 @@
 import { separador } from "./modulos";
-
 export {};
 
 //Exemplos de tipos básicos
@@ -8,14 +7,12 @@ let ativo: boolean = true;
 let nome: string = "Beltrano";
 
 console.log(`${nome} tem ${idade} anos.`);
-
 separador();
 
 // Nova atribuição
 idade = 31;
 console.log(`${nome} agora tem ${idade} anos.`);
 console.log(`Status: ${ativo ? "Ativo" : "Inativo"}`);
-
 separador();
 
 /* Sobre o tipo any(qualquer) 
@@ -25,3 +22,16 @@ qualquerCoisa = 10;
 qualquerCoisa = true;
 
 console.log(`Qualquer coisa: ${qualquerCoisa}`);
+separador();
+
+/* Sobre tipagem implícita ou por inferência */
+let exemplo01 = "Isso é uma string"; // TS infere que é string
+let exemplo02 = 10; // TS infere que é number
+
+// neste caso não tem como o TS inferir nada, portanto fica como ANY **EVITAR**
+let exemplo03; 
+
+// Variável sem valor mas já definida como tipo string 
+let exemplo04: string;
+
+console.log(typeof exemplo01);
--- dist/02-tipagem-basica.js
+++ dist/02-tipagem-basica.js
@@ -18,3 +18,12 @@ let qualquerCoisa = "Pode ser qualquer coisa";
 qualquerCoisa = 10;
 qualquerCoisa = true;
 console.log(`Qualquer coisa: ${qualquerCoisa}`);
+(0, modulos_1.separador)();
+/* Sobre tipagem implícita ou por inferência */
+let exemplo01 = "Isso é uma string"; // TS infere que é string
+let exemplo02 = 10; // TS infere que é number
+// neste caso não tem como o TS inferir nada, portanto fica como ANY **EVITAR**
+let exemplo03;
+// Variável sem valor mas já definida como tipo string 
+let exemplo04;
+console.log(typeof exemplo01);
