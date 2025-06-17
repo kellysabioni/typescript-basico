ee62a27d1128e6614188c19ab55dcbceeb8f5cfe
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-17 11:38:39 -0300

    Atualiza

--- 04-combinando-tipos.ts
+++ 04-combinando-tipos.ts
@@ -18,19 +18,18 @@ separador();
 type NumeroOuTexto = number | string;
 let exemploA: NumeroOuTexto = 2112;
 let exemploB: NumeroOuTexto = "Senac";
-console.log("Exemplo A: " + exemploA );
-console.log("Exemplo B: " + exemploB );
+console.log("Exemplo A: " + exemploA);
+console.log("Exemplo B: " + exemploB);
 
 separador();
 
 type Status = "ativo" | "inativo" | "pendente";
 let situacaoAluno1: Status = "ativo";
-let situacaoAluno2: Status = "inativo"; 
-let situacaoAluno3: Status = "pendente"; 
+let situacaoAluno2: Status = "inativo";
+let situacaoAluno3: Status = "pendente";
 
 console.log(situacaoAluno1);
 console.log(situacaoAluno2);
 console.log(situacaoAluno3);
 
 separador();
-
