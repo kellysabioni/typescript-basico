a6608bc38a71fdcda5d7ec35b9fd07d15f5dabc2
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-17 09:43:01 -0300

    Cria arquivo tipagem basica

---
+++ 02-tipagem-basica.ts
@@ -0,0 +1,20 @@
+import { separador } from "./modulos";
+
+export {};
+
+//Exemplos de tipos básicos
+let idade: number = 30;
+let ativo: boolean = true;
+let nome: string = "Beltrano";
+
+console.log(`${nome} tem ${idade} anos.`);
+
+separador();
+
+// Nova atribuição
+idade = 31;
+console.log(`${nome} agora tem ${idade} anos.`);
+console.log(`Status: ${ativo? "Ativo" : "Inativo"}`);
+
+separador();
+
