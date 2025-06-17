0bde0aa1d12b088ea67d62b37105358c16d7cfa9
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-17 10:32:06 -0300

    Atualiza com tipagem de função

---
+++ 03-funcoes.ts
@@ -0,0 +1,13 @@
+import { separador } from "./modulos";
+
+export {};
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`
+};
+
+console.log(saudacao("Kelly"));
+//console.log(saudacao(true)); 
+//console.log(saudacao(10));
+separador();
+
--- dist/03-funcoes.js
+++ dist/03-funcoes.js
@@ -1,1 +1,11 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const modulos_1 = require("./modulos");
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+;
+console.log(saudacao("Kelly"));
+//console.log(saudacao(true)); 
+//console.log(saudacao(10));
+(0, modulos_1.separador)();
