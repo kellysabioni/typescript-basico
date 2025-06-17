9b8c23e566c6250d101796f1dd868a9a602fd336
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-17 10:38:53 -0300

    Atualiza com exemplo de função

--- 03-funcoes.ts
+++ 03-funcoes.ts
@@ -3,11 +3,21 @@ import { separador } from "./modulos";
 export {};
 
 function saudacao(nome: string): string {
-    return `Olá ${nome}`
-};
+  return `Olá ${nome}`;
+}
 
 console.log(saudacao("Kelly"));
-//console.log(saudacao(true)); 
+//console.log(saudacao(true));
 //console.log(saudacao(10));
 separador();
 
+function saudacaoCompleta(nome: string, curso?: string) {
+    if (curso) {
+        return `Olá ${nome}, você está estudando ${curso}`;
+    }
+    return `Olá ${nome}, sei lá o que você está fazendo...`;
+}
+
+console.log(saudacaoCompleta("João", "TypeScript"));
+console.log(saudacaoCompleta("Paulo"));
+separador();
--- dist/03-funcoes.js
+++ dist/03-funcoes.js
@@ -4,8 +4,16 @@ const modulos_1 = require("./modulos");
 function saudacao(nome) {
     return `Olá ${nome}`;
 }
-;
 console.log(saudacao("Kelly"));
-//console.log(saudacao(true)); 
+//console.log(saudacao(true));
 //console.log(saudacao(10));
+(0, modulos_1.separador)();
+function saudacaoCompleta(nome, curso) {
+    if (curso) {
+        return `Olá ${nome}, você está estudando ${curso}`;
+    }
+    return `Olá ${nome}, sei lá o que você está fazendo...`;
+}
+console.log(saudacaoCompleta("João", "TypeScript"));
+console.log(saudacaoCompleta("Paulo"));
 (0, modulos_1.separador)();
