45d66f329919562206aad299ceaaa9b17b67973a
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-23 09:04:54 -0300

    Solução do exercicio

--- dist/exercicio02.js
+++ dist/exercicio02.js
@@ -1,4 +1,19 @@
 "use strict";
-function exibirProduto({ nome, preco, garantia }) {
-    console.log(`Produto: ${nome} está na oferta por R$${preco} e tem ${garantia} ano(s) de garantia`);
+function mostrarProduto(produto) {
+    console.log("Produto:" + produto.nome);
+    console.log("Produto:" + produto.preco);
+    console.log("Produto:" + produto.garantia);
 }
+const Produto1 = {
+    nome: "TV",
+    preco: 4500,
+    garantia: 1
+};
+const Produto2 = {
+    nome: "Geladeira",
+    preco: 3750,
+    garantia: 2
+};
+mostrarProduto(Produto1);
+console.log("---------------");
+mostrarProduto(Produto2);
--- exercicio02.ts
+++ exercicio02.ts
@@ -2,16 +2,35 @@
 
 /* 1) Crie uma Interface para representar um produto com as propriedades nome, preço e garantia*/
 interface Produto {
-    nome: string;
-    preco: number;
-    garantia: number
+  nome: string;
+  preco: number;
+  garantia: number
 }
 
 /* 2) Crie uma função que receba um objeto baseado na interface Produto e que mostre nome, preço e garantia. Obs.: o objeto deve ser desestruturudo com parâmetros individuais */
-function exibirProduto( {nome, preco, garantia} : Produto) {
-    console.log(`Produto: ${nome}, Preço: ${preco}, Garantia: ${garantia} ano`);    
-}
+/* function mostrarProduto( {nome, preco, garantia} : Produto) {
+  return  console.log(`Produto: ${nome}, Preço: ${preco}, Garantia: ${garantia} ano`);    
+} */
 
+function mostrarProduto(produto: Produto): void {
+  console.log("Produto:" + produto.nome);
+  console.log("Produto:" + produto.preco);
+  console.log("Produto:" + produto.garantia);
+}
 /* 3) Use a função pelo menos 2 vezes passando produtos diferentes */
 
-let produtoA = 
+const Produto1: Produto = {
+  nome: "TV",
+  preco: 4500,
+  garantia: 1
+};
+
+const Produto2: Produto = {
+  nome: "Geladeira",
+  preco: 3750,
+  garantia: 2
+};
+
+mostrarProduto(Produto1);
+console.log("---------------");
+mostrarProduto(Produto2);
